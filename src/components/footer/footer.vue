d5e6bca1416af839990a8bf991b169d6cd4bc595
Sakunthala Jeyaseetha <sakuntha@thoughtworks.com>
2017-10-31 18:25:25 +0530

    GDHI-Tech Basic home page layout <Sakunthala>

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="footer">In footer</div>
+  <div class="footer">
+    <div class="footer-width">
+      <span class="ft-element"><a href="javascript:void(0);">Methodology</a></span>
+      <span class="ft-element"><a href="javascript:void(0);">List of Indicators</a></span>
+      <span class="ft-element"><a href="javascript:void(0);">Contact</a></span>
+    </div>
+
+  </div>
 </template>
 
 <script>
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,6 +1,17 @@
 <template>
+  <div class="header">
+    <div class="header-width">
+      <div class="header-elements">
+        <span class="logo-name hd-element"><a href="javascript:void(0);">Global Digital Health Index</a></span>
+        <span class="hd-element xl-length"><a href="javascript:void(0);">List of Indicators</a></span>
+        <div class="search-box hd-element">
+          <input type="search" placeholder="Search by country name"/>
+        </div>
+      </div>
 
-  <div class="header">In header
+
+
+    </div>
   </div>
 </template>
 
