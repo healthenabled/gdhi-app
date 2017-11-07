b327e27a84265acbfbe2672d1e6332be99754831
Narendar <narenth@thoughtworks.com>
2017-11-07 11:42:30 +0530

    GDHI-Tech Color code to score map  <Naren>

---
+++ src/components/common/color-codes.js
@@ -0,0 +1,12 @@
+export default {
+  getColorCodes () {
+    return [
+      {score: 'NA', color: '#606060'},
+      {score: '1', color: '#BEDCE3'},
+      {score: '2', color: '#77B2BF'},
+      {score: '3', color: '#387886'},
+      {score: '4', color: '#225E8E'},
+      {score: '5', color: '#11184B'}
+    ]
+  }
+}
