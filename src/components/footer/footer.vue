4a4badaa39a8d96ebe012c31a29970c0a78b9e2e
Sakunthala Jeyaseetha <sakuntha@thoughtworks.com>
2017-11-09 14:37:32 +0530

    GDHI-Tech Added date in footer <Sakunthala>

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -4,6 +4,7 @@
       <span class="ft-element"><a href="javascript:void(0);">Methodology</a></span>
       <span class="ft-element"><a href="javascript:void(0);">List of Indicators</a></span>
       <span class="ft-element"><a href="javascript:void(0);">Contact</a></span>
+      <span class="float-right">November 2017</span>
     </div>
 
   </div>
