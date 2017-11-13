659b9fbe664bdfd634a17fe50df3826c92ed5863
Sakunthala Jeyaseetha <sakuntha@thoughtworks.com>
2017-11-13 18:20:49 +0530

    GDHI-12 WIP - Health indicator in country page <Pavithra, Sakunthala>

--- src/components/countryProfile/country-profile.js
+++ src/components/countryProfile/country-profile.js
@@ -1,12 +1,25 @@
 import Vue from 'vue'
 import countryProfile from './countryProfile.html'
+import axios from 'axios'
 
 export default Vue.extend({
+  data () {
+    return {healthIndicatorData: {}}
+  },
   template: countryProfile,
   created () {
-    console.log('IN country profile route')
+    // this.countryCode = this.$route.params.countryCode
+    this.getHealthIndicatorsFor(this.$route.params.countryCode)
   },
   mounted () {
     console.log('IN country profile route')
+  },
+  methods: {
+    getHealthIndicatorsFor (countryCode) {
+      axios.get(`/api/countries/${countryCode}/health_indicators`)
+        .then(response => {
+          this.healthIndicatorData = response.data
+        })
+    }
   }
 })
