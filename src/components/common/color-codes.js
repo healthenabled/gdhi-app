57b7ffd73d99bc4c688419504241ee5ecad6be47
Narendar <narenth@thoughtworks.com>
2017-11-13 12:15:36 +0530

    GDHI-Tech Changing the order of legend numbers and color codes <Naren>

--- src/components/common/color-codes.js
+++ src/components/common/color-codes.js
@@ -2,12 +2,12 @@ export default {
   getColorCodes () {
     // Update variables if changed.
     return [
-      {score: 'NA', color: '#606060'},
-      {score: '1', color: '#BEDCE3'},
-      {score: '2', color: '#77B2BF'},
-      {score: '3', color: '#387886'},
+      {score: '5', color: '#11184B'},
       {score: '4', color: '#225E8E'},
-      {score: '5', color: '#11184B'}
+      {score: '3', color: '#387886'},
+      {score: '2', color: '#77B2BF'},
+      {score: '1', color: '#BEDCE3'},
+      {score: 'NA', color: '#606060'}
     ]
   }
 }
