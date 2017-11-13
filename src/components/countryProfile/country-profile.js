5e525876d7e776e0ed8fabcfa727378f329c2a50
Sakunthala Jeyaseetha <sakuntha@thoughtworks.com>
2017-11-13 19:58:50 +0530

    GDHI-12 Health indicators in coutry page WIP

--- src/components/countryProfile/country-profile.js
+++ src/components/countryProfile/country-profile.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import countryProfile from './countryProfile.html'
 import axios from 'axios'
+import _ from 'lodash'
 
 export default Vue.extend({
   data () {
@@ -8,7 +9,6 @@ export default Vue.extend({
   },
   template: countryProfile,
   created () {
-    // this.countryCode = this.$route.params.countryCode
     this.getHealthIndicatorsFor(this.$route.params.countryCode)
   },
   mounted () {
@@ -19,7 +19,16 @@ export default Vue.extend({
       axios.get(`/api/countries/${countryCode}/health_indicators`)
         .then(response => {
           this.healthIndicatorData = response.data
+          this.hideAllIndicators()
         })
+    },
+    onCategoryExpand (category, index) {
+      category.showIndicator = !category.showIndicator
+    },
+    hideAllIndicators () {
+      _.each(this.healthIndicatorData.categories, (category) => {
+        this.$set(category, 'showIndicator', false)
+      })
     }
   }
 })
