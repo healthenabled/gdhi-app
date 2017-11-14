38317597e1ccd6d31961cc778dbcbc896a1464c6
Narendar <narenth@thoughtworks.com>
2017-11-14 11:48:39 +0530

    GDHI-11 UI table for list of indicators WIP <Naren>

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -2,7 +2,7 @@
   <div class="footer">
     <div class="footer-width">
       <span class="ft-element"><a href="javascript:void(0);">Methodology</a></span>
-      <span class="ft-element"><a href="javascript:void(0);">List of Indicators</a></span>
+      <span class="ft-element"><router-link to="indicatorsInfo">List of Indicators</router-link></span>
       <span class="ft-element"><a href="javascript:void(0);">Contact</a></span>
       <span class="float-right">November 2017</span>
     </div>
