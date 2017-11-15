d008c069e29fde08ef0315ea9c978e9eda716c21
Sonia <soniaj@thoughtworks.com>
2017-11-15 16:26:25 +0530

    GDHI-12 Added Methodology page <Sonia>

--- src/components/methodology/methodology.vue
+++ src/components/methodology/methodology.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>Hello</div>
+  <div class="methodology">
+    <div class="methodology-title">Methodology</div>
+    <div class="content">
+      <p> Our Approach </p>
+      <p>Based on the framework set in the World Health Organization – International Telecommunication Union’s 2012
+      National eHealth Strategy Toolkit, the GDHI team conducted rigorous research and engaged digital health experts
+      from over 20 countries to develop a recommended set of 19 indicators that help countries track, monitor, assess,
+      and benchmark the effective use of digital health.</p>
+      <p>To develop the 19 indicators, the GDHI team:</p>
+      <ul>
+      <li>
+        Conducted a landscape review of global data available through multilateral organizations.
+      </li>
+      <li>
+        Mapped available data by reviewing existing frameworks and tools developed by experts to help practitioners
+        and countries assess the strength and maturity of the enabling environment relevant to digital health.
+      </li>
+      <li>
+        Convened digital health experts to develop use cases for GDHI and refine and define an initial set
+         of recommended indicators into a user-friendly and manageable list.
+       </li>
+     </ul>
+     <p>List of GDHI Indicators. <router-link :to="{path: '/indicatorsInfo'}" class="link">See here</router-link> </p>
+     <p>Data Collection and Verification</p>
+     <p> We work with country partners who are leading digital health efforts in their respective countries to collect
+     data for each country. These country partners submit their country’s data through an annual GDHI questionnaire,
+     and provide scores for each indicator as well as rationale and evidence to support these scores.
+     The GDHI team verifies the collected data before publishing it.</p>
+    </div>
+  </div>
 </template>
 
 <script>
