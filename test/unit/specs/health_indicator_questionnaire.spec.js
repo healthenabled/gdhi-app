0b59cfd18b4443591a825292f10881bacd90e08e
Pavithra <pavithra@thoughtworks.com>
2017-11-23 11:13:19 +0530

    GDHI-TECH Adding babel polyfill <Pavithra>

---
+++ test/unit/specs/health_indicator_questionnaire.spec.js
@@ -0,0 +1,11 @@
+// import Vue from 'vue'
+// import moxios from 'moxios'
+// import Questionnaire from '@/components/health_indicator_questionnaire/health_indicator_questionnaire.js'
+
+describe('Questionnaire.vue', () => {
+  it.only('should populate the health indicators', () => {
+    // const Constructor = Vue.extend(Questionnaire)
+    // const vm = new Constructor().$mount()
+    // console.log(vm.healthIndicators)
+  })
+})
