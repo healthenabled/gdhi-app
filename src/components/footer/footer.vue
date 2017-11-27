6f6a9397908ab6f265bbe800cf6460f8e7ab14ca
Sonia <soniaj@thoughtworks.com>
2017-11-27 16:58:52 +0530

    GDHI-UI Added pipe and respective classes to footer <Sonia>

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="footer">
     <div class="footer-width">
-      <span class="ft-element"><router-link :to="{path: '/methodology'}">Methodology</router-link></span>
+      <span class="ft-element"><router-link :to="{path: '/methodology'}">Methodology</router-link> <span class="ft-element-pipe"> | </span></span>
       <span class="ft-element"><router-link :to="{path: '/indicatorsInfo'}">List of Indicators</router-link></span>
       <span class="float-right"><i>Data For November 2017</i></span>
-      <span class="ft-element float-right"><i>Contact : gdhi@globaldevincubator.org</i></span>
+      <span class="ft-element float-right"><i>Contact : gdhi@globaldevincubator.org</i> <span class="ft-element-pipe"> | </span></span>
     </div>
 
   </div>
