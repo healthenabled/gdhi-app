b2d850ec7bafc5e7186820af5ba1a51df03a1f39
Sakunthala Jeyaseetha <sakuntha@thoughtworks.com>
2017-12-01 16:03:51 +0530

    GDHI-Test Added tests for world map component

--- test/unit/specs/landing-map/world-map.spec.js
+++ test/unit/specs/landing-map/world-map.spec.js
@@ -1,47 +1,115 @@
-// import map from '@/components/landing-map/world-map.js'
-// import eventHandler from '@/components/landing-map/map-event-handler.js'
-// import L from 'leaflet'
-//
-// const countryIndices = [{
-//   'countryId': 'AFG',
-//   'countryName': 'Afghanistan',
-//   'countryPhase': 5,
-//   'overallScore': 4.75,
-//   'colorCode': '#11184B',
-//   'categories': [{
-//     'id': 1,
-//     'name': 'Leadership and Governance',
-//     'overallScore': 4.5,
-//     'phase': 5,
-//     'indicators': [{
-//       'id': 1,
-//       'indicatorDescription': 'Does the country have a separate department / agency / ' +
-//       'national working group for digital health?',
-//       'name': 'Digital health prioritized at the national level through dedicated bodies / ' +
-//       'mechanisms for governance',
-//       'score': 4,
-//       'scoreDescription': 'Governance structure is fully-functional, government-led, consults' +
-//       'with other ministries, and monitors implementation of digital health based on a work' +
-//       'plan.',
-//       'supportingText': null
-//     }]
-//   }]
-// }]
-//
-// const data = {'type':'FeatureCollection', 'features': [
-// {'type':'Feature','geometry':null,'properties':{'scalerank':3,'featurecla':'Admin-0 country','LABELRANK':6,'SOVEREIGNT':'Singapore','SOV_A3':'SGP','ADM0_DIF':0,'LEVEL':2,'TYPE':'Sovereign country','ADMIN':'Singapore','ADM0_A3':'SGP','GEOU_DIF':0,'GEOUNIT':'Singapore','GU_A3':'SGP','SU_DIF':0,'SUBUNIT':'Singapore','SU_A3':'SGP','BRK_DIFF':0,'NAME':'Singapore','NAME_LONG':'Singapore','BRK_A3':'SGP','BRK_NAME':'Singapore','BRK_GROUP':'','ABBREV':'Sing.','POSTAL':'SG','FORMAL_EN':'Republic of Singapore','FORMAL_FR':'','NAME_CIAWF':'Singapore','NOTE_ADM0':'','NOTE_BRK':'','NAME_SORT':'Singapore','NAME_ALT':'','MAPCOLOR7':5,'MAPCOLOR8':3,'MAPCOLOR9':7,'MAPCOLOR13':3,'POP_EST':5888926,'POP_RANK':13,'GDP_MD_EST':487900,'POP_YEAR':2017,'LASTCENSUS':2010,'GDP_YEAR':2016,'ECONOMY':'6. Developing region','INCOME_GRP':'2. High income: nonOECD','WIKIPEDIA':-99,'FIPS_10_':'SN','ISO_A2':'SG','ISO_A3':'SGP','ISO_A3_EH':'SGP','ISO_N3':'702','UN_A3':'702','WB_A2':'SG','WB_A3':'SGP','WOE_ID':23424948,'WOE_ID_EH':23424948,'WOE_NOTE':'Exact WOE match as country','ADM0_A3_IS':'SGP','ADM0_A3_US':'SGP','ADM0_A3_UN':-99,'ADM0_A3_WB':-99,'CONTINENT':'Asia','REGION_UN':'Asia','SUBREGION':'South-Eastern Asia','REGION_WB':'East Asia & Pacific','NAME_LEN':9,'LONG_LEN':9,'ABBREV_LEN':5,'TINY':3,'HOMEPART':1,'MIN_ZOOM':0,'MIN_LABEL':4,'MAX_LABEL':9}},
-// ]}
-//
-// describe('Map comp', () => {
-//   it.only('should get color code for score', () => {
-//     var layer = L.geoJSON(data)
-//     var leafletLayer = sinon.mock(layer)
-//     layer.feature = {'properties': {
-//       'BRK_A3': 'IND'
-//     }}
-//     leafletLayer.expects('setStyle').once().
-//       withArgs({ fillColor: '#606060', fillOpacity: 0.95 })
-//     eventHandler.resetLayer(layer, countryIndices)
-//     leafletLayer.verify()
-//   })
-// })
+import worldMap from '@/components/landing-map/world-map.js'
+import L from 'leaflet'
+
+const countryIndices = [{
+  'countryId': 'AFG',
+  'countryName': 'Afghanistan',
+  'countryPhase': 5,
+  'overallScore': 4.75,
+  'colorCode': '#11184B',
+  'categories': [{
+    'id': 1,
+    'name': 'Leadership and Governance',
+    'overallScore': 4.5,
+    'phase': 5,
+    'indicators': [{
+      'id': 1,
+      'indicatorDescription': 'Does the country have a separate department / agency / ' +
+      'national working group for digital health?',
+      'name': 'Digital health prioritized at the national level through dedicated bodies / ' +
+      'mechanisms for governance',
+      'score': 4,
+      'scoreDescription': 'Governance structure is fully-functional, government-led, consults' +
+      'with other ministries, and monitors implementation of digital health based on a work' +
+      'plan.',
+      'supportingText': null
+    }]
+  }]
+}]
+
+const geoData = {'type': 'FeatureCollection',
+  'features': [
+    {'type': 'Feature',
+      'geometry': null,
+      'properties': {'scalerank': 3,
+        'featurecla': 'Admin-0 country',
+        'SU_A3': 'SGP',
+        'BRK_DIFF': 0,
+        'NAME': 'Singapore',
+        'NAME_LONG': 'Singapore',
+        'BRK_A3': 'SGP',
+        'BRK_NAME': 'Singapore',
+        'BRK_GROUP': '',
+        'ABBREV': 'Sing.'
+      }}
+  ]}
+
+describe('World Map', () => {
+  var layer, mockLayer, callback, callBackSpy, mapStub
+
+  beforeEach(() => {
+    mapStub = sinon.stub(worldMap, 'drawMap').callsFake(function () {
+      this.map = {
+        fitBounds: function () {
+          console.log('fitting bounds')
+        },
+        setView: function () {
+          console.log('setting view')
+        }
+      }
+    })
+    worldMap.drawMap({}, function () {})
+    worldMap.lastClickedCountry = {}
+    layer = L.geoJSON(geoData)
+    mockLayer = sinon.mock(layer)
+
+    callback = function a (arg1) {console.log(arg1)}
+    callBackSpy = sinon.spy(callback)
+  })
+
+  afterEach(() => {
+    assert(mapStub.called)
+    mockLayer.restore()
+    mapStub.restore()
+  })
+
+  it.only('should zoom and pass country name on clicking country', () => {
+    layer.feature = {'properties': {
+      'BRK_A3': 'AFG',
+      'NAME_LONG': 'Afghanistan'
+    }}
+
+    worldMap.handleClick(layer, 'AFG', '', countryIndices, callBackSpy)
+    assert(callBackSpy.calledOnce)
+    expect(callBackSpy.getCall(0).args[0].type).to.equal('COUNTRY')
+    expect(callBackSpy.getCall(0).args[0].countryCode).to.equal('AFG')
+    expect(callBackSpy.getCall(0).args[0].countryName).to.equal('Afghanistan')
+    expect(worldMap.lastClickedCountry).to.equal(layer)
+  })
+
+  it.only('should reset map and pass global on clicking same country', () => {
+    layer.feature = {'properties': {
+      'BRK_A3': 'AFG',
+      'NAME_LONG': 'Afghanistan'
+    }}
+
+    worldMap.handleClick(layer, 'AFG', layer, countryIndices, callBackSpy)
+    assert(callBackSpy.calledOnce)
+    expect(callBackSpy.getCall(0).args[0].type).to.equal('GLOBAL')
+    expect(worldMap.lastClickedCountry).to.equal('')
+  })
+
+  it.only('should reset map and pass country code on clicking unknown country', () => {
+    layer.feature = {'properties': {
+      'BRK_A3': 'SIA',
+      'NAME_LONG': ''
+    }}
+    worldMap.lastClickedCountry = layer
+    worldMap.handleClick('', 'SIA', layer, countryIndices, callBackSpy)
+    assert(callBackSpy.calledOnce)
+    expect(callBackSpy.getCall(0).args[0].type).to.equal('COUNTRY')
+    expect(callBackSpy.getCall(0).args[0].countryCode).to.equal('SIA')
+    expect(callBackSpy.getCall(0).args[0].countryName).to.equal('')
+    expect(worldMap.lastClickedCountry).to.equal('')
+  })
+})
