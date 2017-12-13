cd647a7e595239f85bb654c4a1b51cf7ef0fb9c8
Sonia <soniaj@thoughtworks.com>
2017-12-13 18:49:28 +0530

    GDHI-UI Fixed the casing issue <Sonia>

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import {} from './app.js'
+import {} from './filter.js'
 
 Vue.config.productionTip = false
 
