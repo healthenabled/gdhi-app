ee528faa30ee60fb2a07421e893b9ddc4b666bf4
Pavithra <pavithra@thoughtworks.com>
2017-12-13 15:38:15 +0530

    GDHI-Tech Adding build number to tar file  <Pavithra>

--- create_package.sh
+++ create_package.sh
@@ -2,4 +2,4 @@
 
 build_number=${BUILD_NUMBER-1}
 echo ${build_number}
-npm run build && tar -zvcf gdhi-${build_number}.tar.gz --exclude=*.map --exclude=.DS_Store dist/
+npm run build && tar -zvcf gdhi-1.0.${build_number}.tar.gz --exclude=*.map --exclude=.DS_Store dist/
