65c75bba0098f9ce9da6838591f9345f3f088607
Sonia <soniaj@thoughtworks.com>
2017-12-18 16:39:04 +0530

    GDHI-UI Fixed babel-polyfill error <Sonia>

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -11,8 +11,7 @@ function resolve (dir) {
 
 module.exports = {
   entry: {
-    app: './src/main.js',
-    main: ["babel-polyfill"]
+    app: './src/main.js'
   },
   output: {
     path: config.build.assetsRoot,
