914468c0e93fd17b5b600dc282bd7efb87b86ab4
Markandan <markr@thoughtworks.com>
2018-03-27 15:31:03 +0530

    GDHI-48 Font changes for the homepage - WIP <Mark>

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -3,8 +3,10 @@
     <div class="footer-width">
       <span class="ft-element"><router-link :to="{path: '/methodology'}">Methodology</router-link> <span class="ft-element-pipe"> | </span></span>
       <span class="ft-element"><router-link :to="{path: '/indicators_info'}">List of Indicators</router-link></span>
-      <span class="float-right"><i>Data For November 2017</i></span>
-      <span class="ft-element float-right"><i>Contact : gdhi@globaldevincubator.org</i> <span class="ft-element-pipe"> | </span></span>
+      <span class="ft-element float-right contact">
+        <span class="">Contact : gdhi@globaldevincubator.org <span class="ft-element-pipe"> | </span></span>
+        <span>Data For November 2017</span>
+      </span>
     </div>
 
   </div>
