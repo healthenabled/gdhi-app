4d882b43342d1a2dd913b196d0fe9f792ddd7df5
Markandan <markr@thoughtworks.com>
2018-04-09 17:48:57 +0530

    GDHI-Tech Added tests for country list page <Mark>

--- src/components/countryList/country-list.js
+++ src/components/countryList/country-list.js
@@ -26,8 +26,8 @@ export default Vue.extend({
       this.listCountries(globalHealthIndices.data.countryHealthScores);
     },
     getListOfCountries() {
-      const windowProperties = window.appProperties;
-      return axios.get(`/api/countries_health_indicator_scores?categoryId=${windowProperties.getCategoryFilter()}&phase=${windowProperties.getPhaseFilter()}`);
+      // const windowProperties = window.appProperties;
+      return axios.get('/api/countries_health_indicator_scores?categoryId=&phase=');
     },
     listCountries(countriesDetails) {
       countriesDetails.forEach((country) => {
