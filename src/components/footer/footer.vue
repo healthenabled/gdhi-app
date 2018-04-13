f571be7ad90c2475bb38f97077fc3859c45e56b5
Markandan <markr@thoughtworks.com>
2018-04-13 15:05:34 +0530

    GDHI-UI Removed export from footer <Mark>

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -13,13 +13,6 @@
           :to="{path: '/indicators_info'}"
           class="text-underline" >Indicators
         </router-link>
-        <span class="ft-element-pipe"> | </span>
-      </span>
-      <span class="ft-element sub-header">
-        <a
-          href="/api/export_global_data"
-          class="text-underline">Export Data For All Countries
-        </a>
       </span>
       <span class="ft-element float-right contact">
         <span class="">Contact : gdhi@globaldevincubator.org</span>
