68f8b627bfec006edb6c779f6e92e30949d50fbe
Mahamuni Vinay <vinay.mahamuni@metronom-external.com>
2019-12-10 18:47:10 +0530

    GDHI-UI Add GDHI acronym for consistency across languages <Vinay>

--- src/static-content/indicators/english.js
+++ src/static-content/indicators/english.js
@@ -1,7 +1,7 @@
 export const indicators = {
   gdhi: 'Global Digital Health Index',
   indicatorGuide: 'Indicator Guide',
-  paragraph1: 'The <a class=\'link-blue\' href=\'http://digitalhealthindex.org\' target=\'_blank\'>Global Digital Health Index</a> is an ' +
+  paragraph1: 'The <a class=\'link-blue\' href=\'http://digitalhealthindex.org\' target=\'_blank\'>Global Digital Health Index (GDHI)</a> is an ' +
     'interactive digital resource that tracks, monitors, and evaluates the use of digital technology for health across countries. GDHI uses ' +
     'seven key indicator categories to track progress.',
   indicator: "Indicator"
