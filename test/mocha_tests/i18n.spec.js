a25ccd0b576ff59d392453c8607fa55d7027f8ea
Mahamuni Vinay <vinay.mahamuni@metronom-external.com>
2019-12-19 10:52:17 +0530

    GDHI-UI Add tests to validate Arabic static messages structure <Vinay>

--- test/mocha_tests/i18n.spec.js
+++ test/mocha_tests/i18n.spec.js
@@ -9,10 +9,12 @@ describe('i18n', () => {
     expect(i18n.fallbackLocale).to.equal('en');
   });
 
-  it('should have messages for english, spanish, portuguese, french', () => {
+  it('should have messages for english, spanish, portuguese, french, arabic', () => {
     expect(i18n.messages.en).to.exist;
     expect(i18n.messages.es).to.exist;
     expect(i18n.messages.pt).to.exist;
     expect(i18n.messages.fr).to.exist;
+    expect(i18n.messages.ar).to.exist;
+
   });
 });
